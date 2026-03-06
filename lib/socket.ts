5a5bf628e00fb688e93bbf904ed8cd27c21f931c
rameshdotin <ramesh2041999@gmail.com>
2026-03-06 11:07:53 +0530

    fixes

--- lib/socket.ts
+++ lib/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket | null = null;
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io("https://imramesh.in/", {
+    socket = io("/", {
       autoConnect: false,
     });
   }
